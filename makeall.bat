9e0bec4be002eaaac881f1199d9c7c7ebe9d3be7
Evgeny <evgenyshmidt@gmail.com>
2022-02-23 14:12:15 +0300

    conf

--- makeall.bat
+++ makeall.bat
@@ -1,6 +1,6 @@
-sphinx-build -v -b gettext oneTBB/doc build/gettext 2> log
+sphinx-build -v -b gettext -c . oneTBB/doc build/gettext 2> log
 sphinx-intl update -p build/gettext/ -l ru 2>> log
-sphinx-build -v -b html  -D language=ru oneTBB/doc build/html 2>> log
+sphinx-build -v -b html -c . -D language=ru oneTBB/doc build/html 2>> log
 
 for %%I in (log) do if %%~zI NEQ 0 notepad log
 
