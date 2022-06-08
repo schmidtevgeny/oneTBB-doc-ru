56c00b8e225c0b83c8b56e6e3578894d40ed2ebd
Evgeny <evgenyshmidt@gmail.com>
2022-06-08 22:56:04 +0300

    +

--- makeall.bat
+++ makeall.bat
@@ -1,3 +1,3 @@
-rem sphinx-build -v -b gettext -c . oneTBB/doc build/gettext 2> log
-rem sphinx-intl update -p build/gettext/ -l ru --locale-dir locales 2>> log
+sphinx-build -v -b gettext -c . oneTBB/doc build/gettext 2> log
+sphinx-intl update -p build/gettext/ -l ru --locale-dir locales 2>> log
 sphinx-build -v -b html -c . -D language=ru oneTBB/doc build/html 2>> log
